b41afed320e7fe8908edf30f53f58bbbba98b575
Siva Suthan A <sivasuthan2424@gmail.com>
2026-03-20 14:44:02 +0530

    Update Customer_behavior_query.sql

--- Customer_behavior_query.sql
+++ Customer_behavior_query.sql
@@ -41,15 +41,24 @@ COUNT(*) AS Count_Items
 FROM customer 
 GROUP BY "Size" ORDER BY Count_Items DESC;
 
---Does Discount / Promo Code affect purchase frequency?
---SELECT "Discount_Applied", AVG("Purchase_Frequency_Days") AS Avg_Frequency FROM customer GROUP BY "Discount_Applied";
-SELECT "Promo_Code_Used", AVG("Purchase_Frequency_Days") AS Avg_Frequency FROM customer GROUP BY "Promo_Code_Used";
+--Does Discount Applied affect purchase frequency?
+SELECT "Discount_Applied", 
+AVG("Purchase_Frequency_Days") AS Avg_Frequency 
+FROM customer GROUP BY "Discount_Applied";
 
 --Which Payment Method leads to higher purchase amounts?
-SELECT "Payment_Method", AVG("Purchase_Amount") AS Avg_Amount FROM customer GROUP BY "Payment_Method" ORDER BY Avg_Amount DESC;
+SELECT "Payment_Method", 
+AVG("Purchase_Amount") AS Avg_Amount 
+FROM customer 
+GROUP BY "Payment_Method" ORDER BY Avg_Amount DESC;
 
 --Does Review Rating correlate with repeat purchases?
-SELECT "Review_Rating", AVG("Previous_Purchases") AS Avg_Previous FROM customer GROUP BY "Review_Rating" ORDER BY "Review_Rating" DESC;
+SELECT "Review_Rating", 
+       AVG("Previous_Purchases") AS Avg_Previous 
+       FROM customer GROUP BY "Review_Rating" ORDER BY "Review_Rating" DESC;
 
 --Does Subscription Status affect purchases?
-SELECT "Subscription_Status", AVG("Purchase_Amount") AS Avg_Amount, AVG("Purchase_Frequency_Days") AS Avg_Frequency FROM customer GROUP BY "Subscription_Status";
+SELECT "Subscription_Status", 
+       AVG("Purchase_Amount") AS Avg_Amount, 
+       AVG("Purchase_Frequency_Days") AS Avg_Frequency 
+       FROM customer GROUP BY "Subscription_Status";
